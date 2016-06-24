9e975843d03ed54e7c77a39e00a0ce870e48a26c
shreeramneupane <shreeramneupane0@gmail.com>
2016-06-24 23:37:20 +0545

    RES-VACANCY Create model Vacancy

---
+++ resource/models/vacancy.js
@@ -0,0 +1,22 @@
+'use strict';
+module.exports = function(sequelize, DataTypes) {
+  var Vacancy = sequelize.define('Vacancy', {
+    title: {
+      allowNull: false,
+      type: DataTypes.STRING
+    },
+    description: {
+      allowNull: false,
+      type: DataTypes.TEXT
+    },
+    valid_until: DataTypes.DATE,
+    number_of_opening: DataTypes.INTEGER
+  }, {
+    classMethods: {
+      associate: function(models) {
+        // associations can be defined here
+      }
+    }
+  });
+  return Vacancy;
+};
